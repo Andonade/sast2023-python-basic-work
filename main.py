81b99afe4d4d811cd1f7c66ce8ba4d3084c17c67
Andonade <m18965691135@163.com>
2023-07-21 18:59:19 +0800

    feat:basic features

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import argparse
 import json
-import streamlit as sl
+import sys
+import random
+import re
+
 
 def parser_data():
     """
@@ -8,37 +11,29 @@ def parser_data():
     做出如下约定：
     1. -f 为必选参数，表示输入题库文件
     2. -d 为必选参数，表示是否指定文章
-    3. -t 当-d True为必选参数，表示指定文章的标题
+    3. -t 当-d True时为必选参数，表示指定文章的标题
     """
     parser = argparse.ArgumentParser(
         prog="Word filling game",
         description="A simple game",
         allow_abbrev=True
     )
-
-    parser.add_argument("-f", "--file", help="题库文件", type=str, required=True)
-    parser.add_argument("-d", "--designate", help="是否指定文章", type=bool, required=True)
-    parser.add_argument("-t", "--title", help="指定文章的标题", type=str)
-    # TODO: 添加更多参数
-    
+    parser.add_argument("-f", "--file", help="题库文件", required=True)
+    parser.add_argument("-d", "--designate", help="是否指定文章", required=True)
+    parser.add_argument("-t", "--title", help="指定文章的标题")
     args = parser.parse_args()
     return args
-
 
 
 def read_articles(filename):
     """
     读取题库文件
-
     :param filename: 题库文件名
-
     :return: 一个字典，题库内容
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        # TODO: 用 json 解析文件 f 里面的内容，存储到 data 中
-    
+        data = json.load(f)
     return data
-
 
 
 def get_inputs(hints):
@@ -49,29 +44,27 @@ def get_inputs(hints):
 
     :return: 用户输入的单词
     """
-
     keys = []
     for hint in hints:
         print(f"请输入{hint}：")
-        # TODO: 读取一个用户输入并且存储到 keys 当中
-
+        temp = input()
+        keys.append(temp)
     return keys
 
 
 def replace(article, keys):
     """
     替换文章内容
-
     :param article: 文章内容
     :param keys: 用户输入的单词
-
     :return: 替换后的文章内容
-
     """
-    for i in range(len(keys)):
-        # TODO: 将 article 中的 {{i}} 替换为 keys[i]
-        # hint: 你可以用 str.replace() 函数，也可以尝试学习 re 库，用正则表达式替换
+    pattern = r'{{(\d+)}}'
 
+    def regrex_replace(match):
+        index = int(match.group(1)) - 1
+        return keys[index]
+    article = re.sub(pattern, regrex_replace, article)
     return article
 
 
@@ -79,11 +72,18 @@ if __name__ == "__main__":
     args = parser_data()
     data = read_articles(args.file)
     articles = data["articles"]
-
-    # TODO: 根据参数或随机从 articles 中选择一篇文章
-    # TODO: 给出合适的输出，提示用户输入
-    # TODO: 获取用户输入并进行替换
-    # TODO: 给出结果
-
-
-
+    flag = -1
+    if args.designate == 'True':
+        for i in range(len(articles)):
+            if articles[i]["title"] == args.title:
+                flag = i
+                break
+        if flag == -1:
+            print(f"{args.title} not found")
+            sys.exit()
+    else:
+        flag = random.choice(range(len(articles)))
+        print(f"您抽中了《{articles[flag]['title']}》")
+    keys = get_inputs(articles[flag]["hints"])
+    print("替换结果如下")
+    print(replace(articles[flag]["article"], keys))
